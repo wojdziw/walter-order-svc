a8fe1789bc11d5e10f0a4440e259f6e48fd58137
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-09-09 12:01:16 +0100

    Mandatory hello world plus VSCode debug config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
-
+import { HOST, PORT } from './config';
 
 const app = new Koa();
 app.use(bodyParser());
-
 app.use((ctx) => {
-  console.log(ctx);
+  if (ctx.url !== '/') {
+    ctx.body = `Hello ${ctx.url.slice(1)}!`;
+  } else {
+    ctx.body = 'Hello world!';
+  }
 });
+app.listen(PORT, HOST);
 
